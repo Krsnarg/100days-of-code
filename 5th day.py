33c7d4888934b6f4ec99f9634a8e0d642748aaf4
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-02 08:47:42 +0530

    5th day

---
+++ 5th day.py
@@ -0,0 +1,16 @@
+print("Hey I am a good boy")
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+# Hey Harry, Please don't remove this line.
+# Author: Harry
+# Course: 100days of code
+# print("Hello World")
+print("Hello World")
+
+# This is a 'Single Line Comment'
+print("This is a print statement")
+print("Hello World!!!")  # Printing Hello World
+
+
+print("Hey", 6, 7, sep="~", end="009"\n")
+      print("Harry")
+
