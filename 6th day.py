0bbcf5866401aba2dc1b2d0a211189350fec54cc
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-02 09:14:24 +0530

    6th day

---
+++ 6th day.py
@@ -0,0 +1,30 @@
+a = 123456777783742784675479654832
+print(a)
+Harry = 9
+b = "Harry"
+print(b)
+print(a + b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("the type of c is ", type(c))
+
+a = 1
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+
+
+list1 = [8, 2.3 ,[-4, 5], ["apple", "banana"]]
+print(list1)
+
+
+tuple1 = (("parrot", "sparrow"),("Lion", "Tiger"))
+print(tuple1)
+
+
+dict1 = {"name":"Sakshi", "age":20, "canVote: True"}
+print(dict1)
