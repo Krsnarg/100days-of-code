7a7d82f209cafa00b579a935e7b63a8ae5cb6e90
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-04 09:14:36 +0530

    9th day

---
+++ 9th day.py
@@ -0,0 +1,22 @@
+a = "1"
+b = "2"
+print(a + b)
+
+a = "Harry"
+b = "Bhai"
+print(a + b)
+
+# a = "1"
+# b = "2"
+print(a + b)
+
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit Typecasting
+c = 1.9
+d = 8
+print(c + d)
