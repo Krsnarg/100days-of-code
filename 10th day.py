13a191fb54aee35979346af1ba480b2c8d4ae5b9
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-04 09:21:47 +0530

    10th day

---
+++ 10th day.py
@@ -0,0 +1,11 @@
+a = input()
+print("My name is", a)
+
+a = input("Enter your name: ")
+print("My namme is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+
+print(int(x) + int(y))
