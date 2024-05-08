d6038b35d2aa22efe2c0ef548853520a08b62a8d
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-08 07:09:24 +0530

    12th day

---
+++ 12th day.py
@@ -0,0 +1,19 @@
+names = "Harry,Shubham"
+print(names[0:5])
+print(len(names))
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word.")
+print(mangoLen)
+# print(fruit[0:4])
+# print(fruit[1:4])
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
+
+# Quick Quiz:
+# nm = "Harry"
+# print(nm[-4:-2])
