5e4a28ea6f3c1258923be8b6751d04f1285eb0b2
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-09 17:26:33 +0530

    14th day

---
+++ 14th day.py
@@ -0,0 +1,75 @@
+a = int(input("Enter your age"))
+print("Your age is:", a)
+
+if(a>18):
+    print("You cannot drive")
+
+    # Conditional operators
+    # >, <, >=, <=, ==, !=
+    print(a>18)
+   # print(a<18)
+  #  print(a>=18)
+  #  print(a<=18)
+   # print(a==18)
+   # print(a!=18)
+
+print("You can drive")
+print("Yes")
+
+print("You cannot drive")
+print("No")
+
+print("Yay!")
+
+applePrice= 190
+budget = 200
+if ( budget - ap):
+    print("Alexa, add 1 Kg Apples to the cart.")
+
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+
+    applePrice = 10
+    budget =200
+
+    if( budget - applePrice > 50):
+        print("Alexa, add 1Kg apples to the cart.")
+    elif (budget- applePrice > 70):
+        print("Its okay you can buy")
+    else:
+        print("Alexa, do nor add apples to the cart.")
+
+
+        num = 0
+        if (num < 0):
+            print(("Number is negative"))
+
+        elif (num == 0)
+            print("Number is Zero.")
+        else:
+            print("Number is positive.")
+
+        elif (num == 999):
+print("Number is special")
+
+print("I am happy now")
+
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
+    
+
+        
+
+
+            
