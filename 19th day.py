c6c71250c476a423aacc88a214e9f8d7e3594349
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-18 17:38:33 +0530

    17th, 18th, 19th day

---
+++ 19th day.py
@@ -0,0 +1,41 @@
+for i in range(12):
+    print("5 X", i+1, "=", 5 * i+1)
+    if(i == 10)
+    break
+
+print("Loop ko chhodkr nikal gya")
+
+print("5 X", i+1, "=", 5 * i)+1)
+
+if(i == 10):
+    print("Skip the iteration")
+    continue
+print("5 X", i, "=",5 * i)
+
+
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+
+    i =int(input("Enter the number: "))
+print(i)
+
+print("Done with the Loop")
+
+# count = 5
+# while (count > 0):
+# print(count)
+# count = count - 1
+#else:
+#   print("I am inside else")
+# do {
+# Loop body;
+#  }while(condition);
+
+
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break
