1a907d55f43e0ea04224e83a0bb43d6196e26e73
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-23 20:29:08 +0530

    25th day

---
+++ 25th day.py
@@ -0,0 +1,23 @@
+Countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(Countries)
+temp.append("Russia")    # add item
+temp.pop(3)              # remove item
+temp[2] = "Finland"      # change item
+countries = tuple(temp)
+print(countries)
+
+countries = ("Pakistan", "Afghanistan" "Bangladesh", "ShriLanka")
+countries2 = ("Vietnam", "India", "China")
+southEastAsia = countries + countries2
+print(southEastAsia)
+
+Tuple1 = (0, 1, 2, 3, 2, 3, 1, 3, 2)
+res = Tuple1.count(3)
+print('Count of 3 in Tuple1 is:', res)
+
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+# res = tuple1.count(3)
+# res = tuple1.index(3)
+# res = tuple1.index(311)
+# res = tuple1.index(3, 4, 8)
+res = len(tuple1)
