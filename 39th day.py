c60305a37b56db2e088518aac8af23eca7f05de1
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-31 17:26:26 +0530

    39th day

---
+++ 39th day.py
@@ -0,0 +1,46 @@
+questions = [
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ]
+]
+[
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ]
+]
+[
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ]
+]
+ 
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000,320000]
+
+for i in range(0, len(questions)):
+    questions = questions[i]
+
+    print(f"Question for Rs. {levels[i]}                    b. {questions[2]} ")
+    print(f"a. {questions[1]}                               d. {questions[4]} ")
+
+reply = int(input("Enter your answer (1-4) "))
+if(reply == questions[-1])
+
+print(f"Correct answer, you have won Rs. {levels[i]}")
+else:
+print("Wrong answer!")
+break
+if (i == 4):
+    money = 10000
+else if (i == 9):
+money = 320000
+else:
+elif:
+(i == 14):
+money = 1000000000
+else:
+print("Wrong answer!")
+break
+
+print(f"Your take home moneyis {money}")
+
