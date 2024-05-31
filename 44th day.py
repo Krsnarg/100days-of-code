682c4023d6f9aa876c7efcd8a674b7b26b115c85
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-31 21:04:06 +0530

    44th day

---
+++ 44th day.py
@@ -0,0 +1,39 @@
+import math
+math.floor(4.2343)
+import math
+
+result = math.sqrt(9)
+print(result)      # Output: 3.0
+
+from math import sqrt, pi
+
+result = sqrt(9) * pi
+
+from math import *
+# from math import pi sqrt as s
+import math as m
+
+result = m.sqrt(9) * pi
+
+import math as math_builtin_python
+
+result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+import math
+
+print(dir(math))
+
+print(math.nan, type(math.nan))
+
+def welcome():
+    print("Hey you are welcome my friend")
+
+    harry = "A good boy"
+
+
+from harry import welcome, harry
+import math
+
+print(dir(math))
+print(math.nan, type(math.nan))
+welcome()
+print(harry)
