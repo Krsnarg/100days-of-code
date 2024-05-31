429bfb736c65d4e1779a74c5783f209e75d61a6f
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-05-31 18:00:59 +0530

    40th, 41st & 42nd day

---
+++ 40th day.py
@@ -0,0 +1,14 @@
+# write a Python Programto translate a message into a secret code Language. Use the rules below to translate normal Engish into secret code language.
+
+# Coding:
+# If the word contains atleast 3 characters, remove the first letter and append it at the end
+#    now append the three random characters at the starting and the end
+# else:
+#    simply reverse the string.
+
+# Decoding:
+# if the word contains less than 3 characters, reverse it
+# else:
+#    remove 3 rendom characters from start and end. Now remove the last letter and append it to the beginning
+
+# Your program should ask whether you want to code or decode
