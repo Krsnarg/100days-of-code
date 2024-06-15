574a8094bb03145f208527defa693368e0a39030
Krsnarg <166623610+Krsnarg@users.noreply.github.com>
2024-06-15 14:53:48 +0530

    56th & 57th day

---
+++ 56th day.py
@@ -0,0 +1,29 @@
+def hello():
+    print("hello")
+
+    hello()
+    real = 6
+    img = 8
+
+    sales1 = 6000
+    revenu = 8
+    profit1 = 2000
+    ad1 = 1000
+    # rajeev.sales
+
+    sales2 = 6000
+    profit2 = 2000
+    ad2 = 1000
+   # Vikrant = new Business()
+    # vikrant.sales
+
+    sales3 = 6000
+    profit3 = 2000
+    ad3 = 1000
+
+ #   railwayform   ---> Class [blueprint]
+  #  harry         ---> harry ki info wala form  ---> Object [entity]
+   # tom           ---> tom ki info wala form    ---> Object [entity]
+  #  shubham       ---> shubham ki info wala form  ---> Object [entity]
+ # shubham.changeName("Shubhi") 
+ 
